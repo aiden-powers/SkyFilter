9a81c9390163ecd597a07b8f69a490db3312ff4e
Aiden Powers <powersajbusiness@protonmail.com>
2024-07-06 05:37:17 -1000

    Changed mixin naming to avoid conflict

--- src/client/java/com/example/ExampleModClient.java
+++ src/client/java/com/example/ExampleModClient.java
@@ -6,33 +6,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.text.Text;
 
 public class ExampleModClient implements ClientModInitializer {
-    /** public static void register() {
-        MinecraftClient.getInstance().inGameHud.getChatHud()
-        ServerMessageEvents.ALLOW_CHAT_MESSAGE.register((message, sender, params) -> {
-            // Implement your logic here
-            // For example, block messages containing "forbiddenWord"
-            System.out.println(message.getContent());
-            if (message.getContent().toString().toLowerCase().contains("lowballing")) {
-                System.out.println(message.getContent());
-                sender.sendMessage(Text.literal("MESSAGE BLOCKED CLIENTSIDE, 'lowballing'"), false);
-                return false; // Block the message
-            }
-            return true; // Allow the message
-        });
-        ServerMessageEvents.CHAT_MESSAGE.register((message, sender, params) -> {
-            // Implement your logic here
-            // For example, block messages containing "forbiddenWord"
-            System.out.println(message.toString());
-            if (message.toString().toLowerCase().contains("lowballing")) {
-                System.out.println(message.toString());
-                assert MinecraftClient.getInstance().player != null;
-                MinecraftClient.getInstance().player.sendMessage(Text.literal("Message BLOCKED CLIENTSIDE, 'lowballing'"), false);
-            }
-        });
-    }
-     **/
-
 	@Override
 	public void onInitializeClient() {
+		System.out.println("SkyFilter Client Side");
 	}
 }
